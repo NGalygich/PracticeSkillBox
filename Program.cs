cdda65a4b049a0174d00a738d2ed4e95cf221f82
NGalygich <NGalygich@yandex.ru>
2022-05-15 02:27:54 +0300

    add variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace project{
+    public class Program{
+        static public void Main(){
+            string fullname = "Galyga Nikita Aleksandrovich";
+            int age = 26;
+            string email = "NGalygich@yandex.ru";
+            double points_program = 3.5;
+            double points_math = 2.2;
+            double points_physics = 3.1;
+        }
+    }
+}
